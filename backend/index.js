78194e37acf7872b129bbb0ab8591ff8bdf9b1e0
Nziza-Prince <nzizaprince7@gmail.com>
2024-07-15 01:32:02 +0200

    close to deployment

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,21 @@ require("dotenv").config();
 const todos = require("./apis.js");
 const port = process.env.PORT || 3000;
 const mongoUrl = process.env.MONGO_URI;
-app.use(cors());
+const prodOrigins = [process.env.ORIGIN_1,process.env.ORIGIN_2]
+const devOrigins = ['http://localhost:5173',]
+const allowedOrigins = process.env.NODE_ENV === 'production' ? prodOrigins : devOrigins
+app.use(cors({
+  origin:(origin,callback)=>{
+    if(allowedOrigins.includes(origin)){
+      console.log(origin,allowedOrigins)
+      callback(null,true)
+    }else{
+      callback(new Error('Not allowed'))
+    }
+  },
+  credentials : true,
+  methods : ['GET','POST','PATCH','DELETE']
+}));
 app.use(express.json());
 app.use("/todos", todos);
 mongoose
