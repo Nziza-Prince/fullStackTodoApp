ca44db8f610bd113962e010e8bf30fe1822c487b
Nziza-Prince <nzizaprince7@gmail.com>
2024-07-15 00:57:31 +0200

    removed fiver bullshit

--- myTodoApp/src/main.jsx
+++ myTodoApp/src/main.jsx
@@ -2,9 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {disableReactDevTools} from "@fvilers/disable-react-devtools"
 
-if(import.meta.env.VITE_UBASE_URL === 'production') disableReactDevTools()
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
