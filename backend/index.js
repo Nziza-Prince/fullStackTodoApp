b61b6a2a471eab60470f953006695d69d7a0eecb
Nziza-Prince <nzizaprince7@gmail.com>
2024-07-14 21:43:44 +0200

    fixed the cors

--- backend/index.js
+++ backend/index.js
@@ -2,22 +2,30 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
-require('dotenv').config();
+require("dotenv").config();
 const todos = require("./apis.js");
 const port = process.env.PORT || 3000;
 const mongoUrl = process.env.MONGO_URI;
-
 app.use(cors(
     {
-        origin:['https://full-stack-todo-app-sigma.vercel.app/'],
-        methods:["POST","PATCH","GET","DELETE"]
+        origin:['https://full-stack-todo-app-r9wu.vercel.app'],
+        methods:["POST","PATCH","GET","DELETE"],
+        credentials : true,
+        allowedHeaders: ['Content-Type', 'Authorization']
     }
 ));
+
 app.use(express.json());
 app.use("/todos", todos);
 
-mongoose.connect(mongoUrl)
+mongoose
+  .connect(mongoUrl)
   .then(() => console.log("Connected to MongoDB"))
-  .catch(err => console.error(err));
+  .catch((err) => console.error(err));
 
-app.listen(port, () => console.log(`Server running at http://localhost:${port}`));
+app.listen(port, () =>
+  console.log(`Server running at http://localhost:${port}`)
+);
+app.get("/",(req,res)=>{
+    res.send("yes it worked")
+})
