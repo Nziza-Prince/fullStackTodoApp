a25ea47e4d414b9f9b89d340cc513424422a0b9e
Nziza-Prince <nzizaprince7@gmail.com>
2024-07-14 23:25:14 +0200

    spacing

--- backend/index.js
+++ backend/index.js
@@ -7,10 +7,8 @@ const todos = require("./apis.js");
 const port = process.env.PORT || 3000;
 const mongoUrl = process.env.MONGO_URI;
 app.use(cors());
-
 app.use(express.json());
 app.use("/todos", todos);
-
 mongoose
   .connect(mongoUrl)
   .then(() => console.log("Connected to MongoDB"))
