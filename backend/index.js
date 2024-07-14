7ae086ddbdfd9d1b3c08e707b2b6cd1cc5b55b38
Nziza-Prince <nzizaprince7@gmail.com>
2024-07-14 20:49:54 +0200

    fixed the cors

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,24 @@
-const express = require("express")
-const app = express()
-const mongoose = require("mongoose")
-const cors = require("cors")
-require('dotenv').config()
-const todos = require("./apis.js") 
-const port  = process.env.PORT || 3000
-const mongoUrl = process.env.MONGO_URI
-app.use(cors())
-app.use(express.json())
-app.use("/todos",todos)
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+require('dotenv').config();
+const todos = require("./apis.js");
+const port = process.env.PORT || 3000;
+const mongoUrl = process.env.MONGO_URI;
+
+// Configure CORS to allow requests from specific origins
+const corsOptions = {
+  origin:'https://full-stack-todo-app-sigma.vercel.app/', // Replace '*' with your frontend's URL in production
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use("/todos", todos);
+
 mongoose.connect(mongoUrl)
-.then(()=>console.log("Connected to mongo db"))
-.catch(err =>console.error(err))
-app.listen(port,()=>console.log(`listening at http://localhost:${port}`))
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(err => console.error(err));
+
+app.listen(port, () => console.log(`Server running at http://localhost:${port}`));
