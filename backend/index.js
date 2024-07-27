ed1f9b3090386816494850b039961c955a2bc5d6
Nziza-Prince <nzizaprince7@gmail.com>
2024-07-28 01:18:05 +0200

    made some changes for deployment

--- backend/index.js
+++ backend/index.js
@@ -7,27 +7,9 @@ const todos = require("./apis.js");
 
 const port = process.env.PORT || 3000;
 const mongoUrl = process.env.MONGO_URI;
-
 // Define allowed origins based on NODE_ENV
-const prodOrigins = [
-  "https://full-stack-todo-app-phi.vercel.app/"
-];
-const devOrigins = ['http://localhost:5173'];
-const allowedOrigins = process.env.NODE_ENV === 'production' ? prodOrigins : devOrigins;
-
 // CORS middleware configuration
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed'));
-    }
-  },
-  credentials: true, // You may or may not need this depending on your application's requirements
-  methods: ['GET', 'POST', 'PATCH', 'DELETE']
-}));
-
+app.use(cors());
 app.use(express.json());
 app.use("/todos", todos);
 
